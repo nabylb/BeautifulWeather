7b3849a71d003ea6d11b2f9edbe3cf7cc2051f06
Nabyl Bennouri <nbennouri@car.fit>
2017-09-12 22:52:39 -0700

    New app

--- README.md
+++ README.md
@@ -1,23 +1,67 @@
-#Weather Forecast [![](https://travis-ci.org/brahalla/BeautifulWeather.svg?branch=master)](https://travis-ci.org/brahalla/BeautifulWeather) [![](https://coveralls.io/repos/brahalla/BeautifulWeather/badge.svg?branch=master&service=github)](https://coveralls.io/github/brahalla/BeautifulWeather?branch=master) [![](https://badges.gitter.im/brahalla/BeautifulWeather.svg)](https://gitter.im/brahalla/BeautifulWeather?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
-##About
+# Weather App
 
-[Beautiful Weather](http://weather.danielcottone.com/) is a simple, responsively designed weather forecast app developed with AngularJS.
+ [![Support me on Patreon][badge_patreon]][patreon] [![Buy me a book][badge_amazon]][amazon] [![PayPal][badge_paypal_donate]][paypal-donations] [![Version](https://img.shields.io/npm/v/react-weather-app.svg)](https://www.npmjs.com/package/react-weather-app) [![Downloads](https://img.shields.io/npm/dt/react-weather-app.svg)](https://www.npmjs.com/package/react-weather-app)
 
-## Install Bower
+> A weather app built with React.
 
-Bower is a command line utility. Install it with npm.
+[![react-weather-app](https://i.imgur.com/thRBbbW.png)](https://ionicabizau.github.io/react-weather-app)
 
-Run `npm install -g bower`
+## :cloud: Installation
 
-Bower requires node, npm and git.## Build & development
+```sh
+git clone https://github.com/IonicaBizau/react-weather-app/
+cd react-weather-app
+npm i
+npm start
 
-## Install Grung
+# To build the app for production, run the following command:
+npm run build
+```
 
-Run `npm install -g grunt-cli`
+## :yum: How to contribute
+Have an idea? Found a bug? See [how to contribute][contributing].
 
-Run `npm install && bower install && grunt build` for building and `grunt serve` for preview.
 
-## Testing
+## :sparkling_heart: Support my projects
 
-Running `grunt test` will run the unit tests with karma. Running `grunt e2e` will run end-to-end tests with protractor.
+I open-source almost everything I can, and I try to reply everyone needing help using these projects. Obviously,
+this takes time. You can integrate and use these projects in your applications *for free*! You can even change the source code and redistribute (even resell it).
+
+However, if you get some profit from this or just want to encourage me to continue creating stuff, there are few ways you can do it:
+
+ - Starring and sharing the projects you like :rocket:
+ - [![PayPal][badge_paypal]][paypal-donations]—You can make one-time donations via PayPal. I'll probably buy a ~~coffee~~ tea. :tea:
+ - [![Support me on Patreon][badge_patreon]][patreon]—Set up a recurring monthly donation and you will get interesting news about what I'm doing (things that I don't share with everyone).
+ - **Bitcoin**—You can send me bitcoins at this address (or scanning the code below): `1P9BRsmazNQcuyTxEqveUsnf5CERdq35V6`
+
+    ![](https://i.imgur.com/z6OQI95.png)
+
+Thanks! :heart:
+
+
+## :cake: Thanks
+
+ - I found about the [wunderground.com](https://www.wunderground.com/) API service from [Kim Sarabia (**@kimsarabia**)](https://github.com/KimSarabia). Thanks! :cake:
+ - The icons were created by [**@deiu**](https://github.com/deiu/weather-icons). :cake:
+ - The forecast information is provided by [wunderground.com](https://www.wunderground.com/).
+
+
+
+## :scroll: License
+
+[MIT][license] © [Ionică Bizău][website]
+
+[badge_patreon]: http://ionicabizau.github.io/badges/patreon.svg
+[badge_amazon]: http://ionicabizau.github.io/badges/amazon.svg
+[badge_paypal]: http://ionicabizau.github.io/badges/paypal.svg
+[badge_paypal_donate]: http://ionicabizau.github.io/badges/paypal_donate.svg
+[patreon]: https://www.patreon.com/ionicabizau
+[amazon]: http://amzn.eu/hRo9sIZ
+[paypal-donations]: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RVXDDLKKLQRJW
+[donate-now]: http://i.imgur.com/6cMbHOC.png
+
+[license]: http://showalicense.com/?fullname=Ionic%C4%83%20Biz%C4%83u%20%3Cbizauionica%40gmail.com%3E%20(https%3A%2F%2Fionicabizau.net)&year=2017#license-mit
+[website]: https://ionicabizau.net
+[contributing]: /CONTRIBUTING.md
+[docs]: /DOCUMENTATION.md
---
+++ src/App.js
@@ -0,0 +1,566 @@
+import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import ultraStyles from './ultraStyles'
+import OverlayLoader from 'react-overlay-loading/lib/OverlayLoader'
+import { Flex, Box } from 'grid-styled'
+
+import './App.css';
+
+// const WUNDERGROUND_KEY = "cc915e0f66c67f51";
+
+const ICON_SET = {
+    "Light Drizzle": "rainy",
+    "Light Rain": "rainy",
+    "Light Snow": "snowy",
+    "Light Snow Grains": "snowy",
+    "Light Ice Crystals": "snowy",
+    "Light Ice Pellets": "snowy",
+    "Light Hail": "stormy",
+    "Light Mist": "cloudy",
+    "Light Fog": "cloudy",
+    "Light Fog Patches": "cloudy",
+    "Light Smoke": "cloudy",
+    "Light Volcanic Ash": "cloudy",
+    "Light Widespread Dust": "cloudy",
+    "Light Sand": "cloudy",
+    "Light Haze": "cloudy",
+    "Light Spray": "cloudy",
+    "Light Dust Whirls": "cloudy",
+    "Light Sandstorm": "cloudy",
+    "Light Low Drifting Snow": "snowy",
+    "Light Low Drifting Widespread Dust": "cloudy",
+    "Light Low Drifting Sand": "cloudy",
+    "Light Blowing Snow": "snowy",
+    "Light Blowing Widespread Dust": "cloudy",
+    "Light Blowing Sand": "cloudy",
+    "Light Rain Mist": "cloudy",
+    "Light Rain Showers": "rainy",
+    "Light Snow Showers": "snowy",
+    "Light Snow Blowing Snow Mist": "snowy",
+    "Light Ice Pellet Showers": "snowy",
+    "Light Hail Showers": "stormy",
+    "Light Small Hail Showers": "stormy",
+    "Light Thunderstorm": "stormy",
+    "Light Thunderstorms and Rain": "stormy",
+    "Light Thunderstorms and Snow": "snowy",
+    "Light Thunderstorms and Ice Pellets": "snowy",
+    "Light Thunderstorms with Hail": "stormy",
+    "Light Thunderstorms with Small Hail": "stormy",
+    "Light Freezing Drizzle": "rainy",
+    "Light Freezing Rain": "rainy",
+    "Light Freezing Fog": "cloudy",
+    "Heavy Drizzle": "rainy",
+    "Heavy Rain": "rainy",
+    "Heavy Snow": "snowy",
+    "Heavy Snow Grains": "snowy",
+    "Heavy Ice Crystals": "snowy",
+    "Heavy Ice Pellets": "snowy",
+    "Heavy Hail": "stormy",
+    "Heavy Mist": "cloudy",
+    "Heavy Fog": "cloudy",
+    "Heavy Fog Patches": "cloudy",
+    "Heavy Smoke": "cloudy",
+    "Heavy Volcanic Ash": "cloudy",
+    "Heavy Widespread Dust": "cloudy",
+    "Heavy Sand": "cloudy",
+    "Heavy Haze": "cloudy",
+    "Heavy Spray": "cloudy",
+    "Heavy Dust Whirls": "cloudy",
+    "Heavy Sandstorm": "cloudy",
+    "Heavy Low Drifting Snow": "snowy",
+    "Heavy Low Drifting Widespread Dust": "cloudy",
+    "Heavy Low Drifting Sand": "cloudy",
+    "Heavy Blowing Snow": "snowy",
+    "Heavy Blowing Widespread Dust": "cloudy",
+    "Heavy Blowing Sand": "cloudy",
+    "Heavy Rain Mist": "cloudy",
+    "Heavy Rain Showers": "rainy",
+    "Heavy Snow Showers": "snowy",
+    "Heavy Snow Blowing Snow Mist": "snowy",
+    "Heavy Ice Pellet Showers": "snowy",
+    "Heavy Hail Showers": "stormy",
+    "Heavy Small Hail Showers": "stormy",
+    "Heavy Thunderstorm": "stormy",
+    "Heavy Thunderstorms and Rain": "stormy",
+    "Heavy Thunderstorms and Snow": "stormy",
+    "Heavy Thunderstorms and Ice Pellets": "stormy",
+    "Heavy Thunderstorms with Hail": "stormy",
+    "Heavy Thunderstorms with Small Hail": "stormy",
+    "Heavy Freezing Drizzle": "stormy",
+    "Heavy Freezing Rain": "stormy",
+    "Heavy Freezing Fog": "cloudy",
+    "Patches of Fog": "cloudy",
+    "Shallow Fog": "cloudy",
+    "Partial Fog": "cloudy",
+    "Overcast": "cloudy",
+    "Clear": "sunny",
+    "Partly Cloudy": "rainbow",
+    "Mostly Cloudy": "rainbow",
+    "Scattered Clouds": "rainbow",
+    "Small Hail": "stormy",
+    "Squalls": "cloudy",
+    "Funnel Cloud": "cloudy",
+    "Unknown Precipitation": "rainy",
+    "Unknown": "cloudy",
+    chancesleet: "snowy",
+    chancesnow: "snowy",
+    clear: "sunny",
+    flurries: "snowy",
+    fog: "cloudy",
+    hazy: "cloudy",
+    rain: "rainy",
+    chancerain: "rainy",
+    sleet: "snowy",
+    snow: "snowy",
+    chanceflurries: "snowy",
+    tstorms: "stormy",
+    chancetstorms: "stormy",
+    sunny: "sunny",
+    mostlysunny: "rainbow",
+    partlysunny: "rainbow",
+    partlycloudy: "cloudy",
+    mostlycloudy: "cloudy",
+    cloudy: "cloudy"
+};
+
+const SUPPORTED_LANGUAGES = [
+    "AF", "AL", "AR", "HY", "AZ",
+    "EU", "BY", "BU", "LI", "MY",
+    "CA", "CN", "TW", "CR", "CZ",
+    "DK", "DV", "NL", "EN", "EO",
+    "ET", "FA", "FI", "FR", "FC",
+    "GZ", "DL", "KA", "GR", "GU",
+    "HT", "IL", "HI", "HU", "IS",
+    "IO", "ID", "IR", "IT", "JP",
+    "JW", "KM", "KR", "KU", "LA",
+    "LV", "LT", "ND", "MK", "MT",
+    "GM", "MI", "MR", "MN", "NO",
+    "OC", "PS", "GN", "PL", "BR",
+    "PA", "RO", "RU", "SR", "SK",
+    "SL", "SP", "SI", "SW", "CH",
+    "TL", "TT", "TH", "TR", "TK",
+    "UA", "UZ", "VU", "CY", "SN",
+    "JI", "YI"
+];
+
+function getIcon(icon) {
+    return ICON_SET[icon];
+}
+
+function getTemp (text) {
+    return (text.match(/(\-?[0-9]+)/) || [])[1];
+}
+
+
+class App extends Component {
+
+  constructor (props) {
+      super(props);
+      this.state = {
+        address: '',
+        geocodeResults: null,
+        loading: false
+      }
+      this.handleSelect = this.handleSelect.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+      this.renderGeocodeFailure = this.renderGeocodeFailure.bind(this)
+      this.renderGeocodeSuccess = this.renderGeocodeSuccess.bind(this)
+      var options = {
+    	  enableHighAccuracy: true,
+    	  timeout: 5000,
+    	  maximumAge: 0
+      };
+
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(pos => {
+              this.setState({
+                  coordinates: pos.coords
+              });
+              this.check();
+          }, () => {
+              this.check();
+          }, options);
+      }
+
+      this.check();
+
+      setInterval(() => this.check(), 10 * 60 * 1000);
+  }
+
+  check () {
+      fetch("https://ipinfo.io/json")
+        .then(res => res.json())
+        .then(ip => {
+            let lang = ip.country;
+            if (!SUPPORTED_LANGUAGES.includes(lang)) {
+                lang = "EN";
+            }
+            let crd = this.state.coordinates;
+            crd = crd || {
+                latitude: +ip.loc.split(",")[0]
+              , longitude: +ip.loc.split(",")[1]
+            }
+//            const query = [crd.latitude, crd.longitude].join(",");
+//            const WUNDERGROUND_URL = `https://api.wunderground.com/api/${WUNDERGROUND_KEY}/conditions/lang:${lang}/q/${query}.json`;
+            const ULTRA_URL = `https://q9syafn1oh.execute-api.us-east-1.amazonaws.com/beta/conditions`
+            return fetch(ULTRA_URL, {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  "lat": crd.latitude,
+                  "lon": crd.longitude
+              })
+            })
+        })
+        .then(c => c.json())
+        .then(weather => {
+            this.setState({
+                weather,
+                loading: false
+            });
+        })
+        .catch((error) => {
+           // Handle error here
+           weather: undefined;
+       });
+
+  }
+
+  getWeather(lat, long) {
+            const ULTRA_URL = `https://q9syafn1oh.execute-api.us-east-1.amazonaws.com/beta/conditions`
+            return fetch(ULTRA_URL, {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  "lat": lat,
+                  "lon": long
+              })
+            })
+        .then(c => c.json())
+        .then(weather => {
+            this.setState({
+                weather,
+                loading: false
+            });
+        })
+        .catch((error) => {
+          // Handle error here
+          var a =1;
+        });
+  }
+
+
+renderHeader () {
+    const weather = this.state.weather;
+    var location = weather.city + ", " + weather.country_name;
+    var formated_location = location.toUpperCase();
+
+    const cssClasses = {
+      root: 'form-group',
+      input: 'Demo__search-input',
+      autocompleteContainer: 'Demo__autocomplete-container',
+    }
+
+    const AutocompleteItem = ({ formattedSuggestion }) => (
+        <div className="Demo__suggestion-item">
+          <i className='fa fa-map-marker Demo__suggestion-icon'/>
+          <strong>{formattedSuggestion.mainText}</strong>{' '}
+          <small className="text-muted">{formattedSuggestion.secondaryText}</small>
+        </div>)
+
+    const inputProps = {
+          type: "text",
+          value: this.state.address,
+          onChange: this.handleChange,
+          onBlur: () => { console.log('Blur event!'); },
+          onFocus: () => { console.log('Focused!'); },
+          autoFocus: true,
+          placeholder: formated_location,
+          name: 'Demo__input',
+          id: "my-input-id",
+        }
+
+      return (
+        <div>
+          <img className='logo' src='images/logoHorizontalWhite2.png'/>
+          <Flex p={2} wrap align='right'>
+            	<Box flex='1 1 auto' p={0}>
+            		SHOW ME THE WEATHER IN
+            	</Box>
+            	<Box flex='1 1 auto' p={0}>
+                <PlacesAutocomplete
+                  onSelect={this.handleSelect}
+                  autocompleteItem={AutocompleteItem}
+                  onEnterKeyDown={this.handleSelect}
+                  classNames={cssClasses}
+                  inputProps={inputProps}
+                  styles = {ultraStyles}
+                />
+            	</Box>
+          </Flex>
+          <div className="overlay">
+            <OverlayLoader
+              color={'white'} // default is white
+              loader="ScaleLoader" // check below for more loaders
+              text="Loading... Please wait!"
+              active={this.state.loading}
+              backgroundColor={'red'} // default is black
+              opacity=".4" // default is .9
+              > </OverlayLoader>
+          </div>
+        </div>
+      );
+  }
+
+  renderWeatherToday () {
+      const weather = this.state.weather;
+      var city = "N/A";
+      var country_name = "N/A";
+      var condition = "N/A";
+      var weather_name = "N/A";
+      var uv = "N/A";
+      var local_time = "N/A";
+      if (weather != undefined) {
+         city = weather.city;
+         country_name = weather.country_name;
+         condition = weather.condition;
+         weather_name = weather.weather;
+         uv = weather.uv;
+         local_time = weather.local_time;
+      }
+      var uvi = Math.round(parseFloat(uv));
+
+
+      let icon = getIcon(weather_name);
+      if( icon === undefined ) {
+        icon = "sunny";
+      }
+
+      // Handle empty weather here
+      // xxx
+
+      let hours = parseInt(local_time.split(":")[0]);
+
+      // If it's night then show night icons
+      if ((icon === "sunny" || icon === "clear") && (hours > 20 || hours < 7)) {
+          icon = "starry";
+      }
+
+      // format temperature
+      if (!Number.isInteger(uvi)) {
+        uvi = "?"
+      }
+      if (uvi < 0 ) {
+        uvi = 0;
+      }
+
+      var uviElm = <div className="big-uvi">{uvi}</div>;
+
+
+      var formated_time = city + ", " + country_name;
+      return (
+            <Flex wrap>
+              <Box px={1} py={1} width={1}>
+                <p className="location-description">{formated_time}</p>
+              </Box>
+              <Box px={1} py={1} width={1}>
+                <p className="time-description">{local_time}</p>
+              </Box>
+              <Box p={1} width={1/2}>
+                <div className="icon-wrapper">
+                   <div className={`icon-big ${icon}`} >
+                   </div>
+                   {uviElm}
+                 </div>
+              </Box>
+              <Box p={1} width={1/2}>
+                {uviElm}
+              </Box>
+                <Box p={1} width={1}>
+                <p className="condition-description">{condition}</p>
+              </Box>
+          </Flex>
+      );
+  }
+
+  renderTemperature () {
+      const weather = this.state.weather;
+      const temp = weather.temperatureF;
+      if (temp) {
+          var tempElm = <div className="small-temp">{temp}</div>;
+      }
+
+      return (
+        <Flex>
+        	<Box p={0}>
+        		TEMPERATURE
+        	</Box>
+        	<Box p={0} ml='auto'>
+        		{tempElm}
+        	</Box>
+        </Flex>
+      );
+  }
+
+  renderHumidity () {
+      const weather = this.state.weather;
+      const humidity = weather.rel_humidity.substring(0, weather.rel_humidity.length-1);;
+      if (humidity) {
+          var humidityElm = <div className="small-humidity">{humidity}</div>;
+      }
+
+      return (
+        <Flex>
+        	<Box p={0}>
+        		HUMIDITY
+        	</Box>
+        	<Box p={0} ml='auto'>
+        		{humidityElm}
+        	</Box>
+        </Flex>
+      );
+  }
+
+  renderWind () {
+      const weather = this.state.weather;
+      const wind = weather.wind_speed_mph;
+      if (wind) {
+          var windElm = <div className="small-wind">{wind}</div>;
+      }
+
+      return (
+        <Flex>
+        	<Box p={0}>
+        		WIND
+        	</Box>
+        	<Box p={0} ml='auto'>
+        		{windElm}
+        	</Box>
+        </Flex>
+      );
+  }
+
+  renderProvider () {
+      const weather = this.state.weather;
+      const provider = weather.provider + "-" + weather.method;
+      if (provider) {
+          var providerElm = <div className="small-provider">{provider}</div>;
+      }
+
+      return (
+        <Flex>
+        	<Box p={0}>
+        		SERVICE PROVIDER
+        	</Box>
+        	<Box p={0} ml='auto'>
+        		{providerElm}
+        	</Box>
+        </Flex>
+      );
+  }
+
+renderDay (day, index) {
+      const weather = this.state;
+      const temp = getTemp(weather.temperatureF);
+      if (temp) {
+          var tempElm = <div className="small-temp">{temp}</div>;
+      }
+
+      return (
+            <div className="day" key={index}>
+                <div className="day-description">
+                    {weather.weather}
+                </div>
+                <div className="icon-wrapper">
+                    <div className={`icon-small ${getIcon(weather.weather)}`}>
+                    </div>
+                    {tempElm}
+                </div>
+            </div>
+      );
+  }
+
+  renderWeather () {
+      if (!this.state.weather) {
+          return (
+            <div>
+              <div className="weather-container">
+                  <p>Loading...</p>
+              </div>
+            </div>
+          );
+      }
+      return (
+        <div>
+            <div className="weather-container">
+              {this.renderHeader()}
+              {this.renderWeatherToday()}
+              {this.renderTemperature()}
+              {this.renderHumidity()}
+              {this.renderWind()}
+              {this.renderProvider()}
+          </div>
+        </div>
+      );
+  }
+
+  render() {
+    return (
+        <div>
+          <div className="app">
+                {this.renderWeather()}
+            </div>
+        </div>
+    );
+  }
+
+  handleSelect(address) {
+  this.setState({
+    address,
+    loading: true
+  })
+
+  geocodeByAddress(address)
+    .then((results) => getLatLng(results[0]))
+    .then(({ lat, lng }) => {
+      console.log('Success Yay', { lat, lng })
+      this.setState({
+        geocodeResults: this.renderGeocodeSuccess(lat, lng),
+      })
+    })
+    .catch((error) => {
+      console.log('Oh no!', error)
+      this.setState({
+        geocodeResults: this.renderGeocodeFailure(error),
+      })
+    })
+  }
+
+  handleChange(address) {
+    this.setState({
+      address,
+      geocodeResults: null
+    })
+  }
+
+renderGeocodeFailure(err) {
+  loading: false;
+  return (
+    <div className="alert alert-danger" role="alert">
+      <strong>Error!</strong> {err}
+    </div>
+  )
+}
+
+renderGeocodeSuccess(lat, lng) {
+  this.getWeather(lat,lng);
+}
+
+}
+export default App;
